8218812d2f483f526f3b5ab610ab2896aadde2cc
Gyanendra Singh <gyanendrasingh.eee18@itbhu.ac.in>
2021-09-20 17:00:04 +0530

    Signed-off-by: Gyanendra Singh <gyanendrasingh.eee18@itbhu.ac.in>

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '57508ec4-7e30-4ee5-9630-5bea7bdd3e3a';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
